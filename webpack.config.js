5222282c39fed1fc9ac1c6af6aa75f1edfdccc7f
Miguel Martin <miguel.martin@piksel.com>
2020-12-15 18:09:54 +0100

    change deploy config

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
-        publicPath: '/',
+        publicPath: './',
     },
     resolve: {
         extensions: ['.ts', '.js', '.json', '.tsx', '.jsx'],
